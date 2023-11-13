7944119244431fbfc458ef5d96125ef83e951ad3
PSYCHONOISE <psychonoise@mail.ru>
2023-11-13 07:13:02 +0300

    gr8ytgh5thrb9hgi5tirg

--- brief/js/init.js
+++ brief/js/init.js
@@ -1,4 +1,13 @@
+var collapsibles;
+const arrayRange = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (value, index) => start + index * step);
+
 document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tabs');
+  var instance = M.Tabs.init(elems);
+  var elems = document.querySelectorAll('.collapsible');
+  collapsibles = M.Collapsible.init(elems);
+  //console.log(collapsibles)
+  //collapsibles[1].open()
   var elems = document.querySelectorAll('.tooltipped');
   var instances = M.Tooltip.init(elems);
   var elems = document.querySelectorAll('.parallax');
@@ -8,6 +17,102 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems);
   var rsltmsg = M.Modal.init(document.getElementById('RSLTMSG'));
+});
+
+additionalCosts = {
+  '01': 125,
+  '02': 125,
+  '03': 250,
+  '04': 500,
+  '05': 500,
+  '06': 200,
+  '07': 6,
+  '08': 12,
+  '09': 24,
+  '10': 12,
+  '11': 1,
+  '12': 3,
+  '13': 6,
+  '14': 12,
+  '15': 1,
+  '16': 3,
+  '17': 6,
+  '18': 9,
+  '19': 12,
+  '20': 6,
+  '21': 8,
+  '22': 18,
+  '23': 24,
+  '24': 30,
+  '25': 42,
+  '26': 2,
+  '27': 95,
+  '28': 130,
+  '29': 300,
+  '30': 500,
+  '31': 750,
+  '32': 1500,
+  '33': 250,
+  '34': 12,
+  '35': 15,
+  '36': 2500,
+  '37': 1600,
+  '38': 1300,
+  '39': 100,
+  '40': 50,
+  '41': 300,
+  '42': 300
+}
+
+Array.from(document.getElementsByClassName('splrdChkbx1')).forEach((e) => {
+  let radio = e.children[0];
+  e.addEventListener('click', function() {
+    collapsibles[0].open(1)
+    let inputs = document.querySelectorAll('.service2')
+    for (var i = inputs.length - 1; i >= 0; i--) {
+      if (radio.checked) { inputs[i].checked = false; }
+      else               { inputs[i].checked = true; }
+    }
+    inputs = document.querySelectorAll('.service1')
+    for (var i = inputs.length - 1; i >= 0; i--) {
+      if (radio.checked) { inputs[i].checked = true; }
+      else               { inputs[i].checked = false; }
+    }
+  });
+});
+
+Array.from(document.getElementsByClassName('splrdChkbx2')).forEach((e) => {
+  let radio = e.children[0];
+  e.addEventListener('click', function() {
+    collapsibles[0].open(0)
+    let inputs = document.querySelectorAll('.service1')
+    for (var i = inputs.length - 1; i >= 0; i--) {
+      if (radio.checked) { inputs[i].checked = false; }
+      else               { inputs[i].checked = true; }
+    }
+    inputs = document.querySelectorAll('.service2')
+    for (var i = inputs.length - 1; i >= 0; i--) {
+      if (radio.checked) { inputs[i].checked = true; }
+      else               { inputs[i].checked = false; }
+    }
+  });
+});
+
+Array.from(document.getElementsByClassName('splrdChkbx3')).forEach((e) => {
+  let input1 = e.children[0];
+  let code = input1.getAttribute('name').split('additional')[1]
+  e.addEventListener('click', function() {
+    numCode = Number(code)
+    if (numCode!=document.getElementsByClassName('splrdChkbx3').length) {
+      collapsibles[1].open(numCode)
+    } else {
+      collapsibles[1].open(0)
+    }
+    console.log(numCode-1)
+    let input2 = document.querySelectorAll('[name="fbs'+code+'"]')[0]
+    if (input1.checked) { input2.removeAttribute('disabled'); }
+    else                { input2.setAttribute('disabled', 'disabled'); input2.checked = false; }
+  });
 });
 
 function costCompute() {
@@ -53,4 +158,5 @@ function costCompute() {
     document.getElementById('ALERT').innerText = ''
     document.getElementById('RESULT').innerText = (packagingTypeCost + complectationCost + markingCost + markingOnTheTopPackageCost + taglingCost + visualCheckForDefectsCost + checkingForDefectiveElectricalAppliancesCost + formationOfDeliveryBoxCost * numberOfDeliveryBoxes) + ' руб.'
   }
+
 }
